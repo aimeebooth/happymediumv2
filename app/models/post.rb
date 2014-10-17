5c2abca9f700474467654f3e3cc1c943f520b27c
Aimee Simone <aimeebooth@gmail.com>
2014-10-16 21:12:50 -0400

    Filter Page category posts from main index.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :category
 
+  scope :posts, -> { joins(:category).where.not(categories: {title: 'Page'}) }
+
   def category_title
     category.title unless category.nil?
   end
