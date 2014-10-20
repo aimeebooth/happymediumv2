04fdf3aa8a1baca43677f8f7dbf029c9bcc64302
Aimee Simone <aimeebooth@gmail.com>
2014-10-19 21:19:48 -0400

    Change posts data, temporarily remove posts scope.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :category
 
   # add default category for posts
-  scope :posts, -> { joins(:category).where.not(categories: {title: 'Page'}) }
+  # scope :posts, -> { joins(:category).where.not(categories: {title: 'Page'}) }
 
   def category_title
     category.nil? ? "Uncategorized" : category.title  
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010013549) do
+ActiveRecord::Schema.define(version: 20141020011509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,8 @@ ActiveRecord::Schema.define(version: 20141010013549) do
   end
 
   create_table "posts", force: true do |t|
-    t.string   "name"
     t.string   "title"
-    t.text     "content"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
